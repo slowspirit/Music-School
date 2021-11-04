019aaaf0b1b62e7e131f5f933669473308dd5dfb
slowspirit <slowspirit@gmail.com>
2021-11-04 12:58:02 +0300

    added desktop version

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,24 @@
 $(function () {
-  
+
+  $(".menu a, .footer__logo").on("click", function (event) {
+    event.preventDefault();
+
+    //забираем идентификатор бока с атрибута href
+    var id = $(this).attr('href'),
+
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
+
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+
+  $('.reviews__slider').slick({
+    arrows: true,
+    fade: true,
+    prevArrow: '<button type="button" class="slick-prev"><</button>',
+    nextArrow: '<button type="button" class="slick-next">></button>',
+    autoplay: true,
+    autoplaySpeed: 5000,
+  });
 });
