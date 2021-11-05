8fb48e3721da95d382fea9734e94a5a6e1f6624f
slowspirit <slowspirit@gmail.com>
2021-11-05 13:43:54 +0300

    fixed for production

--- gulpfile.js
+++ gulpfile.js
@@ -89,6 +89,6 @@ exports.browsersync = browsersync;
 exports.watching = watching;
 exports.images = images;
 exports.cleanDist = cleanDist;
-exports.build = series(cleanDist, build);
+exports.build = series(cleanDist, images, build);
 
 exports.default = parallel(styles, scripts, browsersync, watching);
