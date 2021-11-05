4e0f9a8a2ac98eb3fa9cb19acc3bcdf140c43b3b
slowspirit <slowspirit@gmail.com>
2021-11-05 17:51:42 +0300

    impruved adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,7 @@ $(function () {
     $('.menu__list').toggleClass('menu__list--active');
   });
 
-  $(".menu a, .footer__logo").on("click", function (event) {
+  $(".menu a, .header__logo, .footer__logo, .top-btn").on("click", function (event) {
     event.preventDefault();
 
     //забираем идентификатор бока с атрибута href
@@ -19,8 +19,8 @@ $(function () {
   $('.reviews__slider').slick({
     arrows: true,
     fade: true,
-    prevArrow: '<button type="button" class="slick-prev"><</button>',
-    nextArrow: '<button type="button" class="slick-next">></button>',
+    prevArrow: '<button type="button" class="slick-prev"><svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24"><path fill="#d3d3d3" d="M8.5,12.8l5.7,5.6c0.4,0.4,1,0.4,1.4,0c0,0,0,0,0,0c0.4-0.4,0.4-1,0-1.4l-4.9-5l4.9-5c0.4-0.4,0.4-1,0-1.4c-0.2-0.2-0.4-0.3-0.7-0.3c-0.3,0-0.5,0.1-0.7,0.3l-5.7,5.6C8.1,11.7,8.1,12.3,8.5,12.8C8.5,12.7,8.5,12.7,8.5,12.8z"/></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="#d3d3d3" d="M15.54,11.29,9.88,5.64a1,1,0,0,0-1.42,0,1,1,0,0,0,0,1.41l4.95,5L8.46,17a1,1,0,0,0,0,1.41,1,1,0,0,0,.71.3,1,1,0,0,0,.71-.3l5.66-5.65A1,1,0,0,0,15.54,11.29Z"/></svg></button>',
     autoplay: true,
     autoplaySpeed: 5000,
     responsive: [
@@ -28,6 +28,13 @@ $(function () {
         breakpoint: 1100,
         settings: {
           arrows: false,
+          dots: true,
+        }
+      },
+      {
+        breakpoint: 590,
+        settings: {
+          arrows: true,
           dots: true,
         }
       }
